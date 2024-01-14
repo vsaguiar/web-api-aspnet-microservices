4a60b8caba70783b2d7abc6a47599b1b41f190e9
Vinicius Silva <vsaguiar@outlook.com>
2024-01-14 19:07:32 -0300

    repositório para produto

---
+++ VShop.ProductApi/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProductsAsync();
+    Task<Product> GetProductByIdAsync(int id);
+    Task<Product> CreateProductAsync(Product product);
+    Task<Product> UpdateProductAsync(Product product);
+    Task<Product> DeleteProductAsync(int id);
+}
---
+++ VShop.ProductApi/Repositories/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.ProductApi.Context;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly AppDbContext _context;
+    public ProductRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    public async Task<Product> GetProductByIdAsync(int id)
+    {
+        return await _context.Products.Where(c => c.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<Product> CreateProductAsync(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product> UpdateProductAsync(Product product)
+    {
+        _context.Entry(product).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product> DeleteProductAsync(int id)
+    {
+        var product = await GetProductByIdAsync(id);
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+}
