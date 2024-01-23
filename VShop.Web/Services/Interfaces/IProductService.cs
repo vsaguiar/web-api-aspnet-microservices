34c6ac4294a551244d8cbe1519f20804140347f2
Vinicius Silva <vsaguiar@outlook.com>
2024-01-23 15:17:12 -0300

    [VShop.Web]: modelo de domínio e serviços

---
+++ VShop.Web/Services/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using VShop.Web.Models;
+
+namespace VShop.Web.Services.Interfaces;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductViewModel>> GetAllProductsAsync();
+    Task<ProductViewModel> GetProductByIdAsync(int id);
+    Task<ProductViewModel> CreateProductAsync(ProductViewModel productVM);
+    Task<ProductViewModel> UpdateProductAsync(ProductViewModel productVM);
+    Task<bool> DeleteProductByIdAsync(int id);
+}
---
+++ VShop.Web/Services/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using VShop.Web.Models;
+using VShop.Web.Services.Interfaces;
+
+namespace VShop.Web.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IHttpClientFactory _clientFactory;
+    private const string apiEndpoint = "/api/products/";
+    private readonly JsonSerializerOptions _jsonOptions;
+    private ProductViewModel productVM;
+    private IEnumerable<ProductViewModel> listProductsVM;
+
+    public ProductService(IHttpClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory;
+        _jsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true }; // não considera caixa alta e/ou caixa baixa
+    }
+
+    public Task<IEnumerable<ProductViewModel>> GetAllProductsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductViewModel> GetProductByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductViewModel> CreateProductAsync(ProductViewModel productVM)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductViewModel> UpdateProductAsync(ProductViewModel productVM)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteProductByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
