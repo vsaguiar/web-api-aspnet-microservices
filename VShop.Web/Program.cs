3ed6c7e775636dcbe121d5ca17fcbb2cf803039c
Vinicius Silva <vsaguiar@outlook.com>
2024-01-23 15:15:06 -0300

    configurando o serviço do HttpClientFactory

--- VShop.Web/Program.cs
+++ VShop.Web/Program.cs
@@ -9,13 +9,19 @@ namespace VShop.Web
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            #region HttpClientFactory
+            builder.Services.AddHttpClient("ProductApi", c =>
+            {
+                c.BaseAddress = new Uri(builder.Configuration["ServiceUri:ProductApi"]);
+            });
+            #endregion
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
